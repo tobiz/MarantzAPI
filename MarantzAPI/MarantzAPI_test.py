9284a85ecf21161937de21fbf475a84aab335955
pjr <pjrobinson@metronet.co.uk>
2020-07-28 17:26:39 +0100

    Remove use of nc command. Update test script

--- MarantzAPI/MarantzAPI_test.py
+++ MarantzAPI/MarantzAPI_test.py
@@ -19,26 +19,57 @@ avr.write_command("PSFRONT?")
 avr.write_command("PSFRONT A+B")
 avr.write_command("PSFRONT?")
 
-#print ("Phase 2")
+print ("Phase 2")
 
-#avr.amp_assign("ZONE2")
-#avr.write_command("PSFRONT SPA")
-#avr.write_command("PSFRONT?")
-#avr.write_command("PSFRONT SPB")
-#avr.write_command("PSFRONT?")
-#avr.write_command("PSFRONT A+B")
-#avr.write_command("PSFRONT?")
+avr.amp_assign("ZONE2")
+avr.write_command("PSFRONT SPA")
+avr.write_command("PSFRONT?")
+avr.write_command("PSFRONT SPB")
+avr.write_command("PSFRONT?")
+avr.write_command("PSFRONT A+B")
+avr.write_command("PSFRONT?")
 
-#print ("Phase 3")
+print ("Phase 3")
 
-#avr.amp_assign("Front B")
-#avr.write_command("PSFRONT SPA")
-#avr.write_command("PSFRONT?")
-#avr.write_command("PSFRONT SPB")
-#avr.write_command("PSFRONT?")
-#avr.write_command("PSFRONT A+B")
-#avr.write_command("PSFRONT?")   
+avr.amp_assign("Front B")
+avr.write_command("PSFRONT SPA")
+avr.write_command("PSFRONT?")
+avr.write_command("PSFRONT SPB")
+avr.write_command("PSFRONT?")
+avr.write_command("PSFRONT A+B")
+avr.write_command("PSFRONT?") 
 
+print ("Phase 4")
+
+avr.amp_assign('Surround Back')
+avr.write_command("PSFRONT SPA")
+avr.write_command("PSFRONT?")
+avr.write_command("PSFRONT SPB")
+avr.write_command("PSFRONT?")
+avr.write_command("PSFRONT A+B")
+avr.write_command("PSFRONT?") 
+
+print ("Phase 5")
+
+avr.amp_assign('Bi-Amp')
+avr.write_command("PSFRONT SPA")
+avr.write_command("PSFRONT?")
+avr.write_command("PSFRONT SPB")
+avr.write_command("PSFRONT?")
+avr.write_command("PSFRONT A+B")
+avr.write_command("PSFRONT?") 
+
+print ("Phase 6")
+
+avr.amp_assign('Front Height')
+avr.write_command("PSFRONT SPA")
+avr.write_command("PSFRONT?")
+avr.write_command("PSFRONT SPB")
+avr.write_command("PSFRONT?")
+avr.write_command("PSFRONT A+B")
+avr.write_command("PSFRONT?")   
 
 avr.write_command("PWSTANDBY")
+
+print "Test End"
        
